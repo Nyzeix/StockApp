375ea61ca982cdeadb1014be0e543644c59b1d23
Nyzeix <nicolas46.ns@gmail.com>
2025-10-17 13:03:11 +0200

    Ajout de la page de stock et ViewModel (données d'essai)

---
+++ ViewModels/StockViewModel.cs
@@ -0,0 +1,43 @@
+﻿using StockApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+
+namespace StockApp.ViewModels;
+
+class StockViewModel : BaseViewModel, INotifyPropertyChanged
+{
+    // Event de notification de changement de propriété
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    
+    public ObservableCollection<Product> StockItems { get; set; } = new();
+
+    public StockViewModel()
+    {
+        // Chargement des données d'essais, sans passer par une BDD
+        LoadStockItems();
+
+        //ApplyFilters();
+    }
+
+    private void LoadStockItems()
+    {
+        // Exemple de données statiques
+        StockItems.Add(new Product { Name = "Pommes", Quantity = 50, ExpirationDate = 1, Origin = "France", Color = "Rouge", CreatedAt = (int)DateTimeOffset.Now.ToUnixTimeSeconds() });
+        StockItems.Add(new Product { Name = "Bananes", Quantity = 30, ExpirationDate = 1, Origin = "Equateur", Color = "Jaune", CreatedAt = (int)DateTimeOffset.Now.ToUnixTimeSeconds() });
+        StockItems.Add(new Product { Name = "Oranges", Quantity = 20, ExpirationDate = 1, Origin = "Espagne", Color = "Orange", CreatedAt = (int)DateTimeOffset.Now.ToUnixTimeSeconds() });
+    }
+    
+    // Appelle la vue si une propriété évolue
+    private void OnPropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+}
--- Views/StockPage.xaml.cs
+++ Views/StockPage.xaml.cs
@@ -1,3 +1,6 @@
+using StockApp.Models;
+using StockApp.ViewModels;
+
 namespace StockApp.Views;
 
 public partial class StockPage : ContentPage
@@ -5,5 +8,16 @@ public partial class StockPage : ContentPage
 	public StockPage()
 	{
 		InitializeComponent();
+		BindingContext = new StockViewModel();
 	}
+
+	public void OnItemSelected(object sender, SelectionChangedEventArgs e)
+	{
+        if (e.CurrentSelection.FirstOrDefault() is Product selectedProduct)
+        {
+			// Navigate to ProductDetailPage
+			// TODO
+            //Navigation.PushAsync(new ProductDetailPage(selectedProduct));
+        }
+    }
 }
