da8aa3e8bfc3e82dddee10c86304f2f0355c07b1
Nicolas SOUBIGOU <nicolas46.ns@gmail.com>
2026-01-19 16:14:48 +0100

    Ajout du model - ViewModel "Log"

---
+++ Models/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockApp.Models
+{
+    public class Log
+    {
+        string message;
+        int level;
+        int timestamp;
+    }
+}
